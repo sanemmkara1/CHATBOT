7231e9b59754dc175ee2bedbcfb3f787491d4654
sanemmkara1 <132664255+sanemmkara1@users.noreply.github.com>
2023-12-25 05:34:00 -0800

    pdf eklendi

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from langchain.indexes import VectorstoreIndexCreator
 from unidecode import unidecode
 import os
 import json
+from flask_cors import CORS
+
+
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/query": {"origins": "*"}})
 # OpenAI API anahtarı
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 openai_api_key = os.environ.get("OPENAI_API_KEY", None)
 
 # Veri yükleme ve indeks oluşturma
-root_dir = "C:\\Users\\sanemk\\Desktop\\colab\\CHATBOT\\content\\gdrive\\MyDrive" 
+root_dir = "C:\\Users\\Apollo\\Desktop\\colab\\CHATBOT\\content\\gdrive\\MyDrive" 
 pdf_folder_path = os.path.join(root_dir, 'data')
 
 
@@ -35,4 +38,7 @@ def query():
     return jsonify({'result': result_ascii})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+      
+
